6160019d3da1d4176a050921aa1a796257f24e95
Tucker Ritti <45155694+tuckerritti@users.noreply.github.com>
2026-03-13 20:17:45 -0400

    Initial Commit

---
+++ strong-ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  strong-ai
+//
+//  Created by Tucker on 3/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ strong-ai/strong_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  strong_aiApp.swift
+//  strong-ai
+//
+//  Created by Tucker on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct strong_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
